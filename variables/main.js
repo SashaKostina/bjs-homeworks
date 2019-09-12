55a957ee355193a8518cfb94891d9f22325a1a6b
Sasha Kostina <alvakos@gmail.com>
2019-09-12 15:33:00 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,16 @@
+let algebra = 2;
+let geography = 5;
+let physics = 5;
+let midGrade = ((algebra + geography + physics)/3);
+console.log(midGrade);
+
+let name = "Мария Степановна";
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+result = (x * y + 5 * z + x - 1);
+console.log(result);
