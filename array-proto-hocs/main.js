cce9214ee97d1bc783c90163313fdbb4f1dbfea9
Sasha Kostina <alvakos@gmail.com>
2019-10-02 16:04:43 +0300

    1st commit array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,5 +1,4 @@
-
-let compareArrays = function (arr1, arr2){
+function compareArrays (arr1, arr2) {
 	if (arr1.length !== arr2.length) return false;
 	for (let i=0; i<arr1.length; i++){
 		if (arr1[i] !== arr2[i]) return false;
@@ -8,29 +7,27 @@ let compareArrays = function (arr1, arr2){
  }
 
 let arr1 = [1, 2, 4];
-let arr2 = [1, 2, 3];
+let arr2 = [1, 2, 4];
 console.log(compareArrays(arr1, arr2));
 
 
 
-let memoize = function (fn, limit) {
-   	let sum, mSum;
-   	let results = {args, result};
-   	return fn = (a, b) => (a + b); 
-   		if (fn in args) {
-   			console.log();
-   			return results[fn];
-   		}
-   		else {
-   			result = a + b;
-   			results[fn] = result;
+function memoize (fn, limit) {
+	return function () {
+		console.log(fn());
+	}
+
+   	const  sum = (a, b) => (a + b); 
+   
+   args = Array.from(arguments);
+   let result = fn.apply(args);
+   		
    			return result;
 
    		}
-   	}
    	
-   let memory = memoize();
    
-  
   console.log(fn(3, 4));
-  console.log(memoize(3,4));
+  console.log(memoize(3, 4));
+
+
